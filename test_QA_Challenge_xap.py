52a715c50faf159db123a42e6d642e603d0dc8f7
mduniphin <maxx.duniphin@gmail.com>
2017-11-19 15:40:06 -0600

    added a few more tests and test explanation comments

--- test_QA_Challenge_xap.py
+++ test_QA_Challenge_xap.py
@@ -3,11 +3,13 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class XapTextDuplicate(unittest.TestCase):
+class XapTextMirror(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Firefox()
 
+
+    #Tests text input in left textbox
     def test_text_input(self):
         driver = self.driver
         driver.get("https://labs.exaptive.city/xap/78c1ece0-c639-11e7-b984-d7a15f6dfbde?version=0.3.0")
@@ -17,17 +19,65 @@ class XapTextDuplicate(unittest.TestCase):
         assert elem.get_attribute('value') == "test text"
 
 
+    #Tests mirroring of text input in left textbox to right textbox
     def test_text_duplication(self):
         driver = self.driver
         driver.get("https://labs.exaptive.city/xap/78c1ece0-c639-11e7-b984-d7a15f6dfbde?version=0.3.0")
         time.sleep(5)
         elem = driver.find_element_by_xpath("//input[1]")
         elem.send_keys("test text")
-        elem_Right = driver.find_element_by_xpath("//input[2]")
-        assert elem.get_attribute('value') == elem_Right.get_attribute('value')
+        elem_right = driver.find_element_by_xpath("//input[2]")
+        assert elem.get_attribute('value') == elem_right.get_attribute('value')
 
 
-    def test_clear_button_Left(self):
+    #Tests mirroring of text input and backspace key in left textbox to right textbox  
+    def test_text_duplication_backspace(self):
+        driver = self.driver
+        driver.get("https://labs.exaptive.city/xap/78c1ece0-c639-11e7-b984-d7a15f6dfbde?version=0.3.0")
+        time.sleep(5)
+        elem = driver.find_element_by_xpath("//input[1]")
+        elem.send_keys("test text")
+        elem_right = driver.find_element_by_xpath("//input[2]")
+        assert elem.get_attribute('value') == elem_right.get_attribute('value')
+        elem.send_keys(Keys.BACKSPACE)
+        assert elem.get_attribute('value') == "test tex" 
+        assert elem.get_attribute('value') == elem_right.get_attribute('value')
+
+
+    #Confirms that Copy and Paste inputs in left textbox are mirrored to right textbox
+    def test_copy_paste_duplication(self):
+        driver = self.driver
+        driver.get("https://labs.exaptive.city/xap/78c1ece0-c639-11e7-b984-d7a15f6dfbde?version=0.3.0")
+        time.sleep(5)
+        elem = driver.find_element_by_xpath("//input[1]")
+        elem.send_keys(" !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
+        elem_right = driver.find_element_by_xpath("//input[2]")
+        assert elem.get_attribute('value') == " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~" 
+        assert elem_right.get_attribute('value') == " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~" 
+        elem.send_keys(Keys.CONTROL, 'a')
+        elem.send_keys(Keys.CONTROL, 'c')
+        elem.send_keys(Keys.CONTROL, 'v')
+        elem.send_keys(Keys.CONTROL, 'v')
+        time.sleep(2)
+        assert elem.get_attribute('value') == " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~" 
+        assert elem_right.get_attribute('value') == " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~" 
+
+
+    #Confirms left to right textbox mirroring when the right textbox starts with text in it
+    def test_text_right_override(self):
+        driver = self.driver
+        driver.get("https://labs.exaptive.city/xap/78c1ece0-c639-11e7-b984-d7a15f6dfbde?version=0.3.0")
+        time.sleep(5)
+        elem = driver.find_element_by_xpath("//input[1]")
+        elem_right = driver.find_element_by_xpath("//input[2]")
+        elem_right.send_keys("test text")
+        elem.send_keys("This should replace")
+        time.sleep(2)
+        assert elem.get_attribute('value') == elem_right.get_attribute('value')
+
+
+    #Confirms that Clear button clears left textbox
+    def test_clear_button_left(self):
         driver = self.driver
         driver.get("https://labs.exaptive.city/xap/78c1ece0-c639-11e7-b984-d7a15f6dfbde?version=0.3.0")
         time.sleep(5)
@@ -38,16 +88,20 @@ class XapTextDuplicate(unittest.TestCase):
         assert elem.get_attribute('value') == ""
 
 
-    def test_clear_button_Right(self):
+    #Confirms that Clear button clears right textbox
+    def test_clear_button_right(self):
         driver = self.driver
         driver.get("https://labs.exaptive.city/xap/78c1ece0-c639-11e7-b984-d7a15f6dfbde?version=0.3.0")
         time.sleep(5)
         elem = driver.find_element_by_xpath("//input[1]")
         elem.send_keys("test text")
-        elem_Right = driver.find_element_by_xpath("//input[2]")
-        assert elem_Right.get_attribute('value') == "test text" 
+        elem_right = driver.find_element_by_xpath("//input[2]")
+        assert elem_right.get_attribute('value') == "test text" 
         driver.find_element_by_xpath("//button").click()
-        assert elem_Right.get_attribute('value') == ""
+        assert elem_right.get_attribute('value') == ""
+        elem_right.send_keys("test text")
+        driver.find_element_by_xpath("//button").click()
+        assert elem_right.get_attribute('value') == ""
 
 
     def tearDown(self):
